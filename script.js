4f5d04634d2ac27d4684e940801290d382122a87
itslaiba2004 <maryamlaiba2004@gmail.com>
2026-04-25 11:22:45 +0500

    Add functions to manipulate DOM elements

---
+++ script.js
@@ -0,0 +1,15 @@
+function showMessage() {
+  document.getElementById("output").innerText = "Hello from JavaScript!";
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = "lightblue";
+}
+
+function changeText() {
+  document.getElementById("heading").innerText = "Text Changed!";
+}
+
+function changeBox() {
+  document.getElementById("box").style.backgroundColor = "yellow";
+}
